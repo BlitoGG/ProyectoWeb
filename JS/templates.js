8cde25b44de6b46e9f25e2cdce9b4b996b38aedd
AUMNO WEB <AUMNO WEB>
2017-03-30 13:22:27 +0200

    Menu Lateral - OK

--- JS/templates.js
+++ JS/templates.js
@@ -2,18 +2,18 @@ var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/inicio", {
-        templateUrl : "../Templates/inicio.html",
+        templateUrl : "./Templates/inicio.html",
     })
     .when("/info", {
-        templateUrl : "../Templates/info.html",
-        //controller : "londonCtrl"
+        templateUrl : "./Templates/info.html",
     })
     .when("/trabajo1", {
-        templateUrl : "../Templates/trabajo1.html",
-        //controller : "parisCtrl"
+        templateUrl : "./Templates/trabajo1.html",
     })
     .when("/trabajo2", {
-        templateUrl : "../Templates/trabajo2.html",
-        //controller : "parisCtrl"
+        templateUrl : "./Templates/trabajo2.html",
+     })
+    .when("/contacto", {
+        templateUrl : "./Templates/contacto.html",
     });
 });
