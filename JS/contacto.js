0dd2c38b07d7e198b22473bd35dfb9634d93de91
AUMNO WEB <AUMNO WEB>
2017-04-11 14:05:37 +0200

    Contacto.html-Falta Validar

--- JS/contacto.js
+++ JS/contacto.js
@@ -1,40 +1,72 @@
-function limita(maximoCaracteres) {
-    var elemento = document.getElementById("comment");
-    if(elemento.value.length >= maximoCaracteres ) {
-        return false;
-    }
-    else {
-        return true;
-    }
+function limita(elEvento, maximoCaracteres) {
+  var elemento = document.getElementById("comment");
+
+  // Obtener la tecla pulsada
+  var evento = elEvento || window.event;
+  var codigoCaracter = evento.charCode || evento.keyCode;
+  // Permitir utilizar las teclas con flecha horizontal
+  if(codigoCaracter == 37 || codigoCaracter == 39) {
+    return true;
+  }
+
+  // Permitir borrar con la tecla Backspace y con la tecla Supr.
+  if(codigoCaracter == 8 || codigoCaracter == 46) {
+    return true;
+  }
+  else if(elemento.value.length >= maximoCaracteres ) {
+    return false;
+  }
+  else {
+    return true;
+  }
 }
 
-var comentario = document.getElementById("comment").value;
-comentario.addEventListener("keypress",return limita(5));
+function actualizaInfo(maximoCaracteres) {
+    var elemento = document.getElementById("comment");
+    var info = document.getElementById("info");
 
-/*function validacion() {
+    if(elemento.value.length > maximoCaracteres ) {
+        info.innerHTML = "Máximo "+maximoCaracteres+" caracteres";
+        info.classList.remove("text-danger");
+        info.classList.add("text-primary");
+    }
+    else if(elemento.value.length==500){
+        info.innerHTML = "Máximo "+maximoCaracteres+" caracteres";
+        info.classList.remove("text-primary");
+        info.classList.add("text-danger");
+    }
+    else{
+        info.innerHTML = "Puedes escribir hasta "+(maximoCaracteres-elemento.value.length)+" caracteres adicionales";
+        info.classList.remove("text-danger");
+        info.classList.add("text-primary");
+    }
 
-    var usuario = document.getElementById("name").value;
+}
+
+function valida() {
+
+    var usuario = document.getElementById("nombre").value;
     var correo = document.getElementById("mail").value;
     var comentario = document.getElementById("comment").value;
     
-    if( usuario == null || usuario.length == 0 || /^\s+$/.test(usuario) ) {
+    if( usuario == null || usuario.length == 0 || /^([a-zñáéíóú]+[\s]*)+$/.test(usuario) ) {
       // Si no se cumple la condicion...
-      alert('[ERROR] El campo debe tener un valor de...');
+      alert('[ERROR] Nombre');
       return false;
   }
-    else if( correo == null || correo.length == 0 || /^\s+$/.test(correo) ) {
+    else if( correo == null || correo.length == 0 || /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(correo) ) {
       // Si no se cumple la condicion...
-      alert('[ERROR] El campo debe tener un valor de...');
+      alert('[ERROR] Correo');
       return false;
   }
-    else if (condicion que debe cumplir el último campo del formulario) {
+    if( comentario == null || comentario.length == 0 || /^\s+$/.test(comentario) ) {
       // Si no se cumple la condicion...
-      alert('[ERROR] El campo debe tener un valor de...');
+      alert('[ERROR] Comentario');
       return false;
   }
  
     // Si el script ha llegado a este punto, todas las condiciones
     // se han cumplido, por lo que se devuelve el valor true
     return true;
-}*/
+}
 
